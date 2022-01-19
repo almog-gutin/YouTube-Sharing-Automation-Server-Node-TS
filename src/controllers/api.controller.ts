77baaffc45ac441907aa91a2b93e6e2daefccbfb
almoggutin <76820904+almoggutin@users.noreply.github.com>
2022-01-19 02:37:11 +0200

    Added videoSearch function, updated the response data

--- src/controllers/api.controller.ts
+++ src/controllers/api.controller.ts
@@ -5,8 +5,14 @@ import stealthPlugin from 'puppeteer-extra-plugin-stealth';
 
 import * as puppetterUtils from '../utils/puppeteer.utils';
 
-export const shareYouTubeVideos = async (req: Request, res: Response): Promise<void> => {
-    // const { email, videoTitle }: { email: string; videoTitle: string } = req.body;
+export const shareYouTubeVideos = async (req: Request, res: Response): Promise<void | Response> => {
+    const { email, videoTitle }: { email: string; videoTitle: string } = req.body;
+    if (!email || !videoTitle)
+        return res.status(400).send({
+            status: 400,
+            statusText: 'Bad Request',
+            message: 'User must enter an email address and video title.',
+        });
 
     puppeteerExtra.use(stealthPlugin());
     const browser: Browser = await puppeteerExtra.launch({ headless: false });
@@ -14,14 +20,23 @@ export const shareYouTubeVideos = async (req: Request, res: Response): Promise<v
     try {
         const page: Page = await browser.newPage();
 
-        await puppetterUtils.loginToYoutubeAccount(browser, page);
+        await puppetterUtils.loginToYouTubeAccount(browser, page);
+        await page.waitForTimeout(2500);
+
+        const videoSearchResult = await puppetterUtils.videoSearch(browser, page, videoTitle);
+        if (videoSearchResult.type === 'NOT_FOUND')
+            return res.send({
+                status: 200,
+                statusText: 'Ok',
+                message: 'No videos were found.',
+            });
 
         await page.waitForTimeout(5000);
 
-        res.send({ status: 200, message: 'Ok' });
+        res.send({ status: 200, statusText: 'Ok', message: 'Successfully shared the videos.' });
     } catch (err) {
         console.log(err);
-        res.status(500).send({ status: 500, message: 'Internal Server Error' });
+        res.status(500).send({ status: 500, statusText: 'Internal Server Error', message: '' });
     } finally {
         await browser.close();
     }
--- src/utils/puppeteer.utils.ts
+++ src/utils/puppeteer.utils.ts
@@ -1,5 +1,9 @@
 import config from 'config';
-import { Browser, Page } from 'puppeteer';
+import { Browser, ElementHandle, Page } from 'puppeteer';
+
+// Configurations
+const typeOptions = { delay: 100 };
+const waitTime = 1500;
 
 /**
  *
@@ -12,24 +16,59 @@ import { Browser, Page } from 'puppeteer';
  * 2. Inputs the users email and password.
  * 3. Waits 30 seconds to check if the target url is the youtube studio dashboard. At this time, the user should authenticate himself, if MFA is enabled (specifcally the push notifications type).
  */
-export const loginToYoutubeAccount = async (browser: Browser, page: Page): Promise<void> => {
+export const loginToYouTubeAccount = async (browser: Browser, page: Page): Promise<void> => {
     const OAUTH2_GOOGLE_URL: string = config.get('OAUTH2_GOOGLE_URL');
     const EMAIL: string = config.get('EMAIL');
     const PASSWORD: string = config.get('PASSWORD');
     const YOUTUBE_STUDIO_DASHBOARD_URL: string = config.get('YOUTUBE_STUDIO_DASHBOARD_URL');
 
-    if (!OAUTH2_GOOGLE_URL || !EMAIL || !PASSWORD || YOUTUBE_STUDIO_DASHBOARD_URL) throw new Error();
+    if (!OAUTH2_GOOGLE_URL || !EMAIL || !PASSWORD || !YOUTUBE_STUDIO_DASHBOARD_URL) throw new Error();
 
     await page.goto(config.get('OAUTH2_GOOGLE_URL'));
 
-    await page.type('[type="email"]', EMAIL);
-    await page.waitForTimeout(1500);
+    await page.type('[type="email"]', EMAIL, typeOptions);
+    await page.waitForTimeout(waitTime);
 
     await page.click('#identifierNext');
-    await page.waitForTimeout(1500);
+    await page.waitForTimeout(waitTime);
 
-    await page.type('[type="password"', PASSWORD);
+    await page.type('[type="password"', PASSWORD, typeOptions);
     await page.click('#passwordNext');
 
     await browser.waitForTarget((target) => target.url() === YOUTUBE_STUDIO_DASHBOARD_URL);
+};
+
+/**
+ *
+ * @param {Browser} browser
+ * @param {Page} page
+ * @param {string} videoTitle
+ * Recives browser and page puppeteer objects and a string as arguments.
+ *
+ * Steps:
+ * 1. First clicks the content tab in the sidebar which will redirect to the channel's video content.
+ * 2. Selects the filter input and searches for the video title.
+ * 3. The function returns if the videos were found or not, with an appropiate message.
+ */
+export const videoSearch = async (browser: Browser, page: Page, videoTitle: string): Promise<any> => {
+    await page.click('#menu-item-1');
+
+    await page.type('#text-input', videoTitle, typeOptions);
+    const filterInputEl: ElementHandle<HTMLInputElement> | null = await page.$('#text-input');
+    if (!filterInputEl) throw new Error();
+    await filterInputEl.click();
+    await filterInputEl.type(videoTitle);
+    await page.keyboard.press('Enter');
+    await page.waitForTimeout(waitTime);
+
+    const noContentEl: ElementHandle<HTMLInputElement> | null = await page.$(
+        '.no-content style-scope ytcp-video-section-content'
+    );
+    if (!noContentEl) {
+        await browser.close();
+
+        return { type: 'NOT_FOUND', message: `No videos were found with the search: ${videoTitle}` };
+    }
+
+    return { type: 'Found' };
 };
